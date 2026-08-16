82c074b2b90a16bb270a63aa59b25f635d148314
av <alberto.venzo@spiralengineering.it>
2026-08-16 23:01:55 +0200

    add pwm dependencies and struct in the main source code

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/pwm.h>
+
+/* --- PWM --- */
+static const struct pwm_dt_spec ttl =
+  PWM_DT_SPEC_GET(DT_NODELABEL(ttl_pwm));
 
 int main(void)
 {
     int current_main_thread_count = 0;
     int ret;
+
 
     printk("\n--- Testing msleep ---\n");
     k_msleep(5000);
